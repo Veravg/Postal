1fe82cedb847881462762af50f39dca077258d68
Veravg <veravgelder@hotmail.com>
2016-08-11 14:58:30 +0200

    Like posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   #get 'post/index'
 
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 end
